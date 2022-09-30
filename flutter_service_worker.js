f64e0ea7b9dd0cfb0c26530c81e933f2eb974a73
imperio001 <i.rodriguezduran@gmail.com>
2022-09-29 20:53:16 -0500

    Mejoras
    Mejoras solicitadas por Toño

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "4e079cd65fba27b4bc27a6617ba1c784",
-"index.html": "85e2e3ffd41fa5d918967502df9a331e",
-"/": "85e2e3ffd41fa5d918967502df9a331e",
-"main.dart.js": "cfa540557a8b2164f927ad45229dbd93",
+"index.html": "301b9bd58593012b087213a3b461a7ba",
+"/": "301b9bd58593012b087213a3b461a7ba",
+"main.dart.js": "26c3e875a96104cf538889b4e0363535",
 "flutter.js": "f85e6fb278b0fd20c349186fb46ae36d",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
